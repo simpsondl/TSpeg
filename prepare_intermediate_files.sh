35456857fe3509487a37651030e9b17412e92b41
simpsondl <49914433+simpsondl@users.noreply.github.com>
2023-03-31 13:39:32 -0400

    adjusted expected input name and added comments

--- prepare_intermediate_files.sh
+++ prepare_intermediate_files.sh
@@ -2,7 +2,7 @@
 
 # Takes a directly containing two sorted bam files as input
 # Generates seven output files:
-### *readmap : for each bam, file containing readname and reference id
+### *readmap : for each bam, file containing readname and reference id read mapped to
 ### *tokeep : readids not showing recombination and reference id
 ### *pegids : identity of sequenced pegs
 ### *coverage : coverage for detected pegs
@@ -16,15 +16,19 @@ cd $1
 samp=${1##*/}
 
 ### Create readmap files for each bam
+### Uses samtools to view only mapped reads, then pulls relevant columns
 for i in *bam
 do
-	samtools view $i | cut -f1,3 > ${i%%.sort.bam}.readmap
+	samtools view -F 4 $i | cut -f1,3 > ${i%%.bam}.readmap
 done
 
 ### Extract mapped read ids with no recombination
+### Checks that readnames and mapped pegRNA are the same
 paste *readmap | awk '($1 == $3) && ($2 == $4) && ($2 != "*")' | cut -f1,2 > ${samp}_tokeep.txt
 
 ### Determine recombination rate
+### Calculates total number of reads which were mapped and number mapped with no recombination
+### Calculates number with recombination and reports percentage
 mapped=`paste *readmap | awk '($1 == $3) && ($2 != "*") && ($4 != "*")' | wc -l`
 correct=`wc -l ${samp}_tokeep.txt | cut -f1 -d' '`
 recom=`echo "scale=4; 1 - ( $correct / $mapped )" | bc -l`
